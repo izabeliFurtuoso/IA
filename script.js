e4d8ed0b8e802432a1b04d86b4a9ab5b48b99373
izabeliFurtuoso <166657032+izabeliFurtuoso@users.noreply.github.com>
2025-09-15 11:23:33 -0300

    .

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const caixaPerguntas =document.querySelector(".caixa-perguntas");
 const caixaAlternativas =document.querySelector(".caixa-alternativa");
 const caixaResultado =document.querySelector(".caixa-resultado");
 const textoResultado =document.querySelector(".texto-resultado");
+
+const perguntas= [
+{
+enunciados: "Pergunta 1",
+auternativas: [
+    "Alternativa 1",
+    "Alternativa 2"
+]
+},
+
+]
