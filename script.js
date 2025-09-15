3ea2af6c07952012d385dff8bbc29f4b202e7f8d
izabeliFurtuoso <166657032+izabeliFurtuoso@users.noreply.github.com>
2025-09-15 11:28:31 -0300

    .

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-const caixaPrincipal =document.querySelector(".caixa-principal");
-const caixaPerguntas =document.querySelector(".caixa-perguntas");
-const caixaAlternativas =document.querySelector(".caixa-alternativa");
-const caixaResultado =document.querySelector(".caixa-resultado");
-const textoResultado =document.querySelector(".texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativa");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas= [
-{
-enunciados: "Pergunta 1",
-auternativas: [
-    "Alternativa 1",
-    "Alternativa 2"
-]
-},
-
-]
+const perguntas = [
+    {
+        enunciados: "Pergunta 1",
+        auternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciados: "Pergunta 2",
+        auternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciados: "Pergunta 3",
+        auternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciados: "Pergunta 4",
+        auternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciados: "Pergunta 5",
+        auternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
